4b5105f701c81de77fb39843de4c2f346ea49e36
carlosborca <carlosborca@gmail.com>
2017-12-04 15:21:11 -0500

    Modified CrystaLattE to use crystalline benzene at 138K.

--- Sandbox/CrystaLattE.py
+++ Sandbox/CrystaLattE.py
@@ -2,6 +2,6 @@
 
 import Read_CIF
 
-args = ['', '-i', 'ice-Ih.cif', '-o',  'ice-Ih.xyz']
+args = ['', '-i', 'Crystal_Benzene-138K.cif', '-o',  'Crystal_Benzene-138K.xyz', '-b', '5', '5', '5']
 
 Read_CIF.main(args)
