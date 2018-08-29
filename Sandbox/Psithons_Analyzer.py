652e8c60eac2dca8799d7bbcd0d855557296815c
Carlos H. Borca <carlosborca@gmail.com>
2018-08-29 18:58:16 -0400

    Prelimiary working version of the analysis script

--- Sandbox/Psithons_Analyzer.py
+++ Sandbox/Psithons_Analyzer.py
@@ -66,9 +66,8 @@ def success_check(fname, verbose=0):
 
 
 # ======================================================================
-def get_nmer_key(fname, verbose=0):
-    """Takes a Psi4 output file and find the N-mer name to produce a 
-    dictionary key.
+def get_nmer_data(fname, verbose=0):
+    """.
     
     Arguments:
     <str> fname
@@ -80,35 +79,34 @@ def get_nmer_key(fname, verbose=0):
     <str> key
         The key to create a new N-mer dictionary in the dictionary
         containing all N-mers.
+    .
+    .
+    .
+
     """
-    
+
     with open(fname, 'r') as outf:
+        
+        # Start a line counter
+        i = 0
+        
+        # Index to indicate where the N-body decomposition
+        # information starts and ends in the Psi4 output file.
+        nbini = None
+        nbend = None
+
+        n_body_energy = None
+
         for line in outf:
+
+            # Increment the line counter by 1.
+            i += 1
+
             # Find the name of the N-mer, and use it to create
             # the key for its entry in the nmers dictionary.
             if "Psithon input for N-mer:" in line:
                 splt = line[:-1].split(":")
-                key = splt[-1].strip()
-    
-    return key
-# ======================================================================
-
-
-# ======================================================================
-def get_nmer_data(fname, verbose=0):
-    """.
-    
-    Arguments:
-    <str> fname
-        Name of the Psi4 output file to check.
-    <int> verbose
-        Adjusts the level of detail of the printouts.
-    
-    Returns:
-    """
-
-    with open(fname, 'r') as outf:
-        for line in outf:
+                keynmer = splt[-1].strip()
             
             # Find the number of replicas of the N-mer.
             if "Number of replicas:" in line:
@@ -126,21 +124,77 @@ def get_nmer_data(fname, verbose=0):
                 msps = splt[-1].strip()
                 min_monomer_separations = msps.split()
 
-            # Find the list of minimum monomer separations of the N-mer.
+            # Find the list of COM monomer separations of the N-mer.
             if "Minimum COM separations:" in line:
                 splt = line[:-1].split(":")
                 csps = splt[-1].strip()
                 com_monomer_separations = csps.split()
-
-    return replicas, priority_min, min_monomer_separations, com_monomer_separations
+            
+            # Find where the start of the N-Body decomposition 
+            # information is.
+            if "n-Body" in line:
+                nbini = i
+            
+            # Find where the end of the N-Body decomposition 
+            # information is.
+            if nbini:
+                
+                # If the line is blank, record the line number as the
+                # end of the N-Body decomposition block, unless it has
+                # been recorded alredy.
+                if not line.strip():
+                    if nbend:
+                        continue
+                    else:
+                        nbend = i
+                
+                # Take the N-Body energy and the number of monomers in
+                # the N-mer from the last line of the N-Body block. This
+                # number is in Kcal/mol and is now converted to KJ/mol
+                elif not nbend and i > nbini:
+                    lastl = line.strip().split()
+                    number_of_monomers = int(lastl[0])
+                    n_body_energy = float(lastl[-1]) * 4.184 # Same value as in qcdb.psi_cal2J
+                    #print(line[:-1]) #debug
+            
+    return keynmer, number_of_monomers, replicas, priority_min, min_monomer_separations, com_monomer_separations, n_body_energy
     
 # ======================================================================
 
+# ======================================================================
+def print_results(results, crystal_lattice_energy, verbose=1):
+    """Prints a summary of the energy results at the end of the
+    execution.
+    
+    Arguments:
+    <int> verbose
+        Adjusts the level of detail of the printouts.
+    """
+    
+    if verbose >= 1:
+        print("Summary of results:")
+        print("---------------------------+--------------+------+--------------+---------------+--------------+----------------------------------------------------------------------")
+        print("                           | Non-Additive | Num. |        N-mer | Partial Crys. |  Calculation | Minimum Monomer")
+        print("N-mer Name                 |    MB Energy | Rep. | Contribution | Lattice Ener. |     Priority | Separations")
+        print("                           |     (KJ/mol) |  (#) |     (KJ/mol) |      (KJ/mol) | (Arb. Units) | (A)")
+        print("---------------------------+--------------+------+--------------+---------------+--------------+----------------------------------------------------------------------")
+        for result in results:
+            print(result)
+        print("---------------------------+--------------+------+--------------+---------------+--------------+----------------------------------------------------------------------")
+        print("\nCrystal Lattice Energy (Eh)       = {:5.8f}".format(crystal_lattice_energy * 2625.500))
+        print("Crystal Lattice Energy (KJ/mol)   = {:9.8f}".format(crystal_lattice_energy))
+        print("Crystal Lattice Energy (Kcal/mol) = {:9.8f}\n".format(crystal_lattice_energy / 4.184))
+# ======================================================================
 
-if __name__ == "__main__":
+
+# ======================================================================
+def main(verbose=1):
 
     # A dictionary for storing N-mers will be created.
     nmers = {}
+
+    crystal_lattice_energy = 0.0
+    results = []
     
     d = os.getcwd()
     
@@ -150,30 +204,69 @@ if __name__ == "__main__":
         #NOTE: This is prone to error if the extension was changed.
         if f.endswith(".out"):
             
-            print(os.path.join(d, f)) #debug
+            #print(os.path.join(d, f)) #debug
 
             # Check if Psi4 exited successfully.
             # Why bothering analyzing a file otherwise?
             success = success_check(f)
-            print(success) #debug
+            #print(success) #debug
     
             if success:
                 # If the output was ran successufully, get the N-mer name
-                # to create a key for its soon-to-be created dictionary.
-                keynmer = get_nmer_key(f)
-                print(keynmer) #debug
+                # to create a key for its soon-to-be created dictionary
+                # and get the data to populate such N-mer dictionary.
+                keynmer, number_of_monomers, replicas, priority_min, min_monomer_separations, com_monomer_separations, n_body_energy = get_nmer_data(f)
 
                 # Create a new dictionary for the current N-mer.
                 nmers[keynmer]= {}
-                print(nmers) #debug
 
                 # Populate the current N-mer dictionary with the data
                 # from the Psi4 output file.
-                replicas, priority_min, min_monomer_separations, com_monomer_separations = get_nmer_data(f)
-                print(replicas) #debug
-                print(priority_min) #debug
-                print(min_monomer_separations) #debug
-                print(com_monomer_separations) #debug
+                nmers[keynmer]["replicas"] = replicas
+                nmers[keynmer]["priority_min"] = priority_min
+                nmers[keynmer]["min_monomer_separations"] = min_monomer_separations
+                nmers[keynmer]["com_monomer_separations"] = com_monomer_separations
+                nmers[keynmer]["nambe"] = n_body_energy 
+                nmers[keynmer]["contrib"] = n_body_energy * replicas / number_of_monomers
+                
+                crystal_lattice_energy += nmers[keynmer]["contrib"]
+
+                #print(n_body_energy) #debug
+                #print(replicas) #debug
+                #print(number_of_monomers) #debug
+                #print(nmers[keynmer]["contrib"]) #debug
+                #print(crystal_lattice_energy) #debug
+                
+                # Generate a string with an ordered list of minimum separations
+                # between atoms belonging to different monomers.
+                rminseps = ""
+                
+                nmer_min_monomer_separations = nmers[keynmer]["min_monomer_separations"] 
+                nmer_min_monomer_separations.sort()
+                
+                for r in nmer_min_monomer_separations:
+                    rminseps += "{:6.3f} ".format(float(r))
+
+                nmer_result = "{:26} | {:>12.8f} | {:>4} | {:>12.8f} | {:>13.8f} | {:12.6e} | {}".format(
+                        keynmer,
+                        nmers[keynmer]["nambe"],
+                        nmers[keynmer]["replicas"],
+                        nmers[keynmer]["contrib"],
+                        crystal_lattice_energy,
+                        nmers[keynmer]["priority_min"],
+                        rminseps)
+
+                #print(nmer_result)
+                
+                results.append(nmer_result)
 
         else:
             continue
+    
+    print_results(results, crystal_lattice_energy)
+
+    #print("\nThe N-mers Dictionary looks like this:\n") #debug
+    #print(nmers) #debug
+
+if __name__ == "__main__":
+    main()
