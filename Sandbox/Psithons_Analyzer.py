13083100472d8e9892a852d94bc8f15b0b062550
Carlos H. Borca <carlosborca@gmail.com>
2018-08-29 12:36:12 -0400

    Analyzer can now get keys of N-mers from outputs

--- Sandbox/Psithons_Analyzer.py
+++ Sandbox/Psithons_Analyzer.py
@@ -34,45 +34,90 @@
 
 # Import standard Python modules.
 import os
-import subprocess
 
-# A dictionary for storing N-mers will be created.
-nmers = {}
 
-d = os.getcwd()
+# ======================================================================
+def success_check(fname, verbose=0):
+    """Takes a Psi4 output file and checks if it contains the string
+    'coffee' to decide if the calculation was successful or not.
 
-for f in os.listdir(d):
+    Arguments:
+    <str> fname
+        Name of the Psi4 output file to check.
+    <int> verbose
+        Adjusts the level of detail of the printouts.
+    
+    Returns:
+    <bool> beer
+        True if the calculation was successful, false otherwise.
+    """
 
-    # Find output files using the default extension.
-    # WARNING: This is prone to error if the extension was changed.
-    if f.endswith(".out"):
-        
-        print(os.path.join(d, f))
-
-        # Check if Psi4 exited successfully.
-        # Why bothering analyzing a file otherwise?
-        beer = False
-        with open(f, 'r') as outf:
-
-            for line in outf:
-                if "Psi4 exiting successfully. Buy a developer a beer!" in line:
-                    beer = True
-                    break
-                else:
-                    continue
-        
-        # If the output was ran successufully, get the data.
-        if beer:
-            with open(f, 'r') as outf:
-                print(f)
-                for line in outf:
-                    if "Psithon input for N-mer:" in line:
-                        print(line)
+    beer = False
+    with open(fname, 'r') as outf:
+        for line in outf:
+            if "Psi4 exiting successfully. Buy a developer a beer!" in line:
+                beer = True
+                break
             
-            print("Success!")
+            else:
+                continue
+    return beer
+# ======================================================================
+
+
+# ======================================================================
+def get_nmer_key(fname, verbose=0):
+    """Takes a Psi4 output file and find the N-mer name to produce a 
+    dictionary key.
+    
+    Arguments:
+    <str> fname
+        Name of the Psi4 output file to check.
+    <int> verbose
+        Adjusts the level of detail of the printouts.
+    
+    Returns:
+    <str> key
+        The key to create a new N-mer dictionary in the dictionary
+        containing all N-mers.
+    """
+    
+    with open(fname, 'r') as outf:
+        for line in outf:
+            # Find the name of the N-mer, and use it to create
+            # the key for its entry in the nmers dictionary.
+            if "Psithon input for N-mer:" in line:
+                splt = line[:-1].split(":")
+                key = splt[-1].strip()
+    
+    return key
+# ======================================================================
+
+
+if __name__ == "__main__":
+
+    # A dictionary for storing N-mers will be created.
+    nmers = {}
+    
+    d = os.getcwd()
+    
+    for f in os.listdir(d):
+    
+        # Find output files using the default extension.
+        # WARNING: This is prone to error if the extension was changed.
+        if f.endswith(".out"):
             
+            print(os.path.join(d, f)) #debug
+
+            # Check if Psi4 exited successfully.
+            # Why bothering analyzing a file otherwise?
+            success = success_check(f)
+            print(success) #debug
+    
+            # If the output was ran successufully, get the N-mer name
+            # to create a key for its soon-to-be created dictionary..
+            keynmer = get_nmer_key(f)
+            print(keynmer) #debug
+
         else:
-            print("Failure!")
-
-    else:
-        continue
+            continue
