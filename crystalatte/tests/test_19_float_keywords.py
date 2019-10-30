2bee0323a8fb13a4aefbb6356af305b7e2d6fe29
Carlos H. Borca <carlosborca@gmail.com>
2019-10-29 20:58:47 -0400

    Add input parser tests

---
+++ crystalatte/tests/test_19_float_keywords.py
@@ -0,0 +1,17 @@
+"""
+Unit and regression test for the crystalatte package.
+"""
+
+# Import package, test suite, and other packages as needed
+import crystalatte
+import pytest
+import sys
+
+def test_wrong_input():
+    """
+    ."""
+
+    # Execute 
+    with pytest.raises(SystemExit):
+        with pytest.raises(IndexError):
+            crystalatte.input_parser("crystalatte/data/Floats.cle")
