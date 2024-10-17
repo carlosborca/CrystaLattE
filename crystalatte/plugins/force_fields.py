af6f94bce00e832d0fe747d71416285a20fc061d
Austin M. Wallace <50717545+Awallace3@users.noreply.github.com>
2024-10-17 15:38:43 -0400

    Custom energy functions (#10)
    * first example generated
    
    * pip installable and custom force field function example
    
    * custom_energy functions
    
    * rm setup.py and added dependencies to pyproject.toml
    
    * rm pandas dep and updated qcel molecule creatoin
    
    * reset Ammonia.xyz

---
+++ crystalatte/plugins/force_fields.py
@@ -0,0 +1,53 @@
+import numpy as np
+import qcelemental as qcel
+
+
+def example_energy_function(
+    qcel_mol: qcel.models.Molecule,
+    cif_output: str,
+    nmers: dict,
+    keynmer: str,
+    nmer: dict,
+    rminseps: str,
+    rcomseps: str,
+    cle_run_type: list,
+    method="method_name_if_applicable",
+    bsse_type=None,
+    job_memory=None,
+    verbose=0,
+    **kwargs,
+):
+    """
+    Every crystalatte energy function plugin must accept the above arguments.
+
+    Takes the `nmers` dictionary; `keynmer`, the key of a given N-mer of
+    the N-mers dictionary;
+
+    Results are stored in the `nmer` dictionary under the key `nambe` standing
+    for non-additive many-body energy.
+
+    kwargs passed to crystalatte.main() are passed to the energy function
+    allowing the user to specify any additional arguments.
+
+    Questions: Why are there no charge or multiplictity arguments anywhere in the code?
+    """
+    example_arg = kwargs.get("example_extra_arg", 0.0)
+    print(f"Example extra argument: {example_arg}")
+    print(qcel_mol)
+    n_body_energy = -0.0105 * np.random.rand()
+    if len(nmer["monomers"]) > 2:
+        n_minus_1_body_energy = -0.0005
+        nmer["nambe"] = n_body_energy - n_minus_1_body_energy
+
+    else:
+        nmer["nambe"] = n_body_energy
+    return
+
+
+def main():
+    example_energy_function()
+    return
+
+
+if __name__ == "__main__":
+    main()
